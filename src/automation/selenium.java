b39beb7ab4b8b1ca71087f7cd14d1df6e66d48a1
kaustubh0588 <kaustubh.shahane@gmail.com>
2020-05-08 11:23:05 +0530

    push it to branch

--- src/automation/selenium.java
+++ src/automation/selenium.java
@@ -1,31 +1,56 @@
 package automation;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class selenium {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) throws InterruptedException  {
+		
 		System.setProperty("webdriver.chrome.driver","/Users/beigkaustubhs/Downloads/jdk-14.0.1.jdk/chromedriver");
 		WebDriver driver= new ChromeDriver();
 		driver.get("http://demo.guru99.com/test/newtours/register.php");
-		driver.manage().window().maximize();
-		driver.findElement(By.name("firstname")).sendKeys("Kaustubh");
-		//driver.findElement(By.name("lastname")).sendKeys("Shahane");
-		//driver.findElement(By.name("phone")).sendKeys("9884788440");
-		//driver.findElement(By.name("userName")).sendKeys("kaustubh.shahane@gmail.com");
-		//driver.findElement(By.name("address1")).sendKeys("Chennai");
-		//driver.findElement(By.name("city")).sendKeys("Chennai");
-		//driver.findElement(By.name("state")).sendKeys("Tamil Nadu");
-		//driver.findElement(By.name("postalCode")).sendKeys("600010");	
-		driver.findElement(By.name("country")).sendKeys("AUSTRALIA");
-		//driver.findElement(By.name("email")).sendKeys("kaustubh.shahane@gmail.com");
-		//driver.findElement(By.name("password")).sendKeys("Chennai123");
-		//driver.findElement(By.name("confirmPassword")).sendKeys("Chennai123");
-
+        Thread.sleep(10000);
+		driver.get("http://demo.guru99.com/test/newtours/");
+		Thread.sleep(10000);
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println("Total links are " + links.size());
+		for (int i = 0; i < links.size(); i++) {
+		WebElement element = links.get(i);
+		String url = element.getAttribute("href");
+		verifyLink(url);
 		
-	}
+				}
+			}
 
-}
+
+	public static void verifyLink(String urlLink) {
+
+	try {
+		URL link = new URL(urlLink);
+		HttpURLConnection httpConn = (HttpURLConnection) link.openConnection();
+		httpConn.setConnectTimeout(2000);
+		httpConn.connect();
+		if (httpConn.getResponseCode() == 200) {
+		System.out.println(urlLink + " - " + httpConn.getResponseMessage());
+				  }
+		if (httpConn.getResponseCode() == 404) {
+		System.out.println(urlLink + " - " + httpConn.getResponseMessage());
+				}
+			}
+		
+	    catch (Exception e) {
+					
+				}
+			}
+
+		}
+
+
